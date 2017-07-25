bf5294e410728f703de243ea361ed09fd0c9432d
nejdetckenobi <nejdetyucesoy@gmail.com>
2017-07-25 17:11:53 +0300

    Moving to PyPI

--- setup.py
+++ setup.py
@@ -9,7 +9,12 @@ module = Extension('medioex',
                    )
 
 setup(name='PyMedIOEx',
-      version='1.0',
-      description='This is a demo package',
+      packages=['PyMedIOEx'],
+      version='1.1',
+      description='A Python3 wrapper for MedIOEx for Raspberry Pi 3',
+      author='Nejdet Çağdaş Yücesoy / Ülkü Bayraktar',
+      author_email='nejdetyucesoy@gmail.com',
+      url='https://github.com/nejdetckenobi/pymedioex',
+      download_url='https://github.com/nejdetckenobi/pymedioex/archive/1.1.tar.gz',
       ext_modules=[module])
 

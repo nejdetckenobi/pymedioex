a75383a049f00d8e91872d6e5987a780f6c45451
nejdetckenobi <nejdetyucesoy@gmail.com>
2017-07-27 10:54:13 +0300

    Fixed custom header problem

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ module = Extension('medioex',
                    sources=['medioex.c', 'pmedex.c'],
                    libraries=['bcm2835'],
                    library_dirs=['./'],
+                   include_dirs=['./'],
                    extra_compile_args=['-std=gnu11', '-lbcm2835'],
-                   extra_link_args = ['-std=gnu11']
+                   extra_link_args=['-std=gnu11']
                    )
 
 setup(name='PyMedIOEx',
-      version='1.1',
+      version='1.4',
       description='A Python3 wrapper for MedIOEx for Raspberry Pi 3',
       author='Nejdet Çağdaş Yücesoy / Ülkü Bayraktar',
       author_email='nejdetyucesoy@gmail.com',
       url='https://github.com/nejdetckenobi/pymedioex',
-      download_url='https://github.com/nejdetckenobi/pymedioex/archive/1.1.tar.gz',
       ext_modules=[module])
-
